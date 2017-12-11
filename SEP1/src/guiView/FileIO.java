50e0f5a6d3b51d95f952914e8d371db11c1671ef
Amahdya Andrea Delkescamp (256523 ICT) <256523@VIAUC.DK>
2017-12-11 11:36:07 +0100

    made changes to FileIO

--- SEP1/src/guiView/FileIO.java
+++ SEP1/src/guiView/FileIO.java
@@ -1,16 +1,58 @@
 package guiView;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
-public class FileIO implements Serializable {
-	private VIAModel viaModel1 = new VIAModel();
-	
-	public void setVIAModel() {
-		
-//		viaModel1 = FILE IO RECEIVED OBJECT HERE
-	}
-	public void setToFile(VIAModel viaModel) {
-		
-	}
+public class FileIO implements Serializable
+{
+   private VIAModel viaModel1 = new VIAModel();
+
+   public void setVIAModel() throws IOException, ClassNotFoundException
+   {
+
+      FileInputStream fstream = new FileInputStream("viaModel.bin");
+      ObjectInputStream inputFile = new ObjectInputStream(fstream);
+
+      System.out.println("Reading from file...");
+
+      viaModel1 = (VIAModel) inputFile.readObject();
+      inputFile.close();
+
+      // viaModel1 = FILE IO RECEIVED OBJECT HERE
+   }
+
+   public void setToFile() throws IOException
+   {
+
+      FileOutputStream fstream = new FileOutputStream("viaModel.bin");
+      ObjectOutputStream outputFile = new ObjectOutputStream(fstream);
+
+      System.out.println("Writing to file...");
+      try
+      {
+         outputFile.writeObject(viaModel1);
+         outputFile.close();
+         System.out.println("Done!");
+      }
+
+      catch (FileNotFoundException e)
+      {
+         System.out.println("File not found.");
+      }
+
+      catch (IOException ioe)
+      {
+         System.out.println("Error.");
+      }
+
+   }
 
 }
