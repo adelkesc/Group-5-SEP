60326b03a9ae98df375c1bcb85d2ac8ebe02367a
mouhanadeem <31960661+mouhanadeem@users.noreply.github.com>
2017-12-11 10:37:04 +0100

    hopefully this wont fuck up

---
+++ SEP1/src/guiView/EventsList.java
@@ -0,0 +1,42 @@
+package guiView;
+
+import java.util.ArrayList;
+
+public class EventsList {
+ArrayList<Events> eventsArrLi;
+
+public EventsList() {
+	eventsArrLi = new ArrayList<>();
+}
+public void addEvent(Events event) {
+	eventsArrLi.add(event);
+}
+public void removeEvent() {
+	
+}
+public Events editEvent(Events event) {
+	Events tempEvent = new Events();
+	tempEvent.setName(event.getName());
+	tempEvent.setDate(event.getDate());
+	tempEvent.setDuration(event.getDuration());
+	tempEvent.setType(event.getType());
+	tempEvent.setLocation(event.getLocation());
+	tempEvent.setCategory(event.getCategory());
+	tempEvent.setPrice(event.getPrice());
+	tempEvent.setMinPartic(event.getMinPartic());
+	tempEvent.setMaxPartic(event.getMaxPartic());
+	tempEvent.setFinalized(event.isFinalized());	
+	return tempEvent;
+}
+public Events searchByName(String name) {
+	for (int i = 0; i < eventsArrLi.size(); i++) {
+		if (eventsArrLi.get(i).getName().equals(name)){
+			return eventsArrLi.get(i);
+		}
+	}
+	return null;
+}
+public Events getEvent(int i) {
+	return eventsArrLi.get(i);
+}
+}
---
+++ SEP1/src/guiView/VIAModel.java
@@ -0,0 +1,25 @@
+package guiView;
+
+public class VIAModel {
+//	public VIAModel(Events event, Members member, Lecturer lecturer) {
+//		
+//	}
+//   public void writeBinaryFile() throws IOException
+//   {
+//      
+//   }
+//   
+//   public EventsList readBinaryFile() throws IOException, ClassNotFoundException
+//   {
+//      
+//   }
+//   public LecturerList readBinaryFile() throws IOException, ClassNotFoundException
+//   {
+//      
+//   }
+//   public MemberList readBinaryFile() throws IOException, ClassNotFoundException
+//   {
+//      
+//   }
+
+}
